e6e61238b0c935b87c50dfe4a9e2ed2df0bef638
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-04-14 14:52:48 -0700

    add more comments

--- assets/movie-script.js
+++ assets/movie-script.js
@@ -1,20 +1,20 @@
 //using AOS library to animate
 AOS.init();
-var moviesReceived = false;
 var allMovies=[];
 var total_movies = 0;
 
 // function sends ajax request to treaming-availability server to retrieve movies
 function pickMovie(genre, keywords){
-    
+    // Show loader and hide emojis and present movies sections
     $(".loader").css("display","block");
     $("#chooseEmojis").css("display","none");
     $("#presentMovies").css("display", "block");
 
-
+    // Loop through all the keywords to get movies for each keyword
     for (var j=0; j<keywords.length; j++){
             
         var keyword = keywords[j];
+        // Set up the AJAX call to the API endpoint with the necessary parameters
         var settings = {
                 "async": false,
                 "crossDomain": true,
@@ -30,10 +30,10 @@ function pickMovie(genre, keywords){
             
             var response = JSON.parse(data);
             var results = response.result;
-  
+             // Hide loader
             $(".loader").css("display","none");
+            // Add the total number of movies received to the count
             total_movies += results.length;
-            moviesReceived = true;
       
             //The API may occasionally produce duplicate results,
             //so we can get rid of duplicates   
@@ -48,16 +48,15 @@ function pickMovie(genre, keywords){
             }   
        //storing past movies in the local storage
         localStorage.setItem("movieList", JSON.stringify(allMovies));
-
+        // Show the start over button
         $('#startOver-btn').css("display", "block")
-
-        
         });
 
     }
 
     // no movies for the chosen keyword has been found => present the user with a default oprion.
     if (total_movies == 0){
+        // Set the parameters for the API request
         var settings = {
                 "async": true,
                 "crossDomain": true,
@@ -67,61 +66,65 @@ function pickMovie(genre, keywords){
                     "X-RapidAPI-Key": "b3dbc942ddmsh8add83241714b1ap1ace8ajsn8339705327d6",
                     "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com"
                 }
-            };
-                //if no moves then get the best rated movie
-            $.ajax(settings).done(function (data) {
+        };
 
-                var response = JSON.parse(data);
-                var results = response.result;
-                var bestMoviesIds = 0;
-                var secondBestMoviesIds = 0;
-                var moviesToPresent = [0];
-                var bestMovieRating = results[0].imdbRating;
+        // Send the API request and handle the response
+        $.ajax(settings).done(function (data) {
+            // Parse the JSON response
+            var response = JSON.parse(data);
+            // Get the list of movie results from the response
+            var results = response.result;
+            // Initialize variables to track the best movies
+            var bestMoviesIds = 0;
+            var secondBestMoviesIds = 0;
+            var moviesToPresent = [0];
+             // Set the initial best movie rating to the rating of the first result
+            var bestMovieRating = results[0].imdbRating;
 
-                // retieve movies with the chosen genre and choose two of them with the best raiting
-                if (results.length>1){
-
-                    
-                    for (var i = 0; i < results.length; i++) {
-                        var movie = results[i];
-                        if (movie.imdbRating > bestMovieRating){
-                            secondBestMoviesIds = bestMoviesIds;
-                            bestMoviesIds = i;
-                        }
+            // retieve movies with the chosen genre and choose two of them with the best raiting
+            if (results.length>1){                    
+                for (var i = 0; i < results.length; i++) {
+                    var movie = results[i];
+                     // Add the movie to the list of all movies and display it
+                    if (movie.imdbRating > bestMovieRating){
+                        secondBestMoviesIds = bestMoviesIds;
+                        bestMoviesIds = i;
                     }
-                    moviesToPresent = [secondBestMoviesIds, bestMoviesIds];
-
                 }
-                //The API may occasionally produce duplicate results,
-                //so we can get rid of duplicates
-                var titles = [];
-                moviesToPresent.forEach(element =>
-                    {
-                        var movie = results[element];
-                        if (!titles.includes(movie.title)){
-                            titles.push(movie.title);
-                            allMovies.push(movie);
-                            displayMovie(movie);
-                        }
-                        
-                    }
-                );
+                moviesToPresent = [secondBestMoviesIds, bestMoviesIds];
 
-                    
-                localStorage.setItem("movieListGenres", JSON.stringify(allMovies));
-                $('#startOver-btn').css("display", "block")
+            }
+            //The API may occasionally produce duplicate results,
+            //so we can get rid of duplicates
+            var titles = [];
+            moviesToPresent.forEach(element =>
+               {
+                    var movie = results[element];
+                    if (!titles.includes(movie.title)){
+                        titles.push(movie.title);
+                        allMovies.push(movie);
+                        displayMovie(movie);
+                    }
+                        
+                }
+            );
+
+            // Store the list of all movies in local storage        
+            localStorage.setItem("movieListGenres", JSON.stringify(allMovies));
+            // Display the "start over" button
+            $('#startOver-btn').css("display", "block")
              
-            });    
+        });    
 
     }
 }
 
-// This function takes a movie object as a parameter and displays its details in a tile format
-// It first generates a comma-separated list of genres from the 'genres' array in the movie object
-// Then it creates various HTML elements to build the tile layout
-// The layout can be changed by modifying the 'is-half', 'is-one-third', etc. classes in the 'tile' element
-// Finally, it sets animation attributes using the 'data-aos' attributes of aos library to add a flip effect to the tile
-// The tile is then appended to the 'movies' element in the HTML document
+/*This function takes a movie object as a parameter and displays its details in a tile format
+It first generates a comma-separated list of genres from the 'genres' array in the movie object
+Then it creates various HTML elements to build the tile layout
+The layout can be changed by modifying the 'is-half', 'is-one-third', etc. classes in the 'tile' element
+Finally, it sets animation attributes using the 'data-aos' attributes of aos library to add a flip effect to the tile
+The tile is then appended to the 'movies' element in the HTML document*/
 function displayMovie(movie){
     movieGenres = "";
             
